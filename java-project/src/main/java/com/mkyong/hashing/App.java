8fbc59dd505e2ae9ff4e48077f83772854aeeb85
mkyong <mkyong2002@gmail.com>
2018-09-21 14:49:45 +0800

    add java project

---
+++ java-project/src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,13 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
