e653c96720c154b0a90d246050c82c9f8c340008
Junlapong L <junlapong@gmail.com>
2019-05-31 04:28:45 +0700

    fix missing WEB-INF/views/index.html

--- java-multi-modules/web/src/test/java/com/mkyong/web/TestWelcome.java
+++ java-multi-modules/web/src/test/java/com/mkyong/web/TestWelcome.java
@@ -13,36 +13,29 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#integration-testing-annotations-junit-jupiter
-//@ExtendWith(SpringExtension.class)
-//@WebAppConfiguration
-//@ContextConfiguration(classes = SpringConfig.class)
 @SpringJUnitWebConfig(SpringConfig.class)
 @DisplayName("Test Spring MVC default view")
 public class TestWelcome {
 
-    private MockMvc mockMvc;
+	private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext webAppContext;
+	@Autowired
+	private WebApplicationContext webAppContext;
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
-    }
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+	}
 
-    @Test
-    public void testDefault() throws Exception {
+	@Test
+	public void testDefault() throws Exception {
 
-        this.mockMvc.perform(
-                get("/"))
-                //.andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"))
-                .andExpect(model().attribute("query", "123456"));
-        //.andExpect(model().attribute("sha256", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
-        //.andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
+		this.mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"))
+				.andExpect(model().attribute("query", "123456"));
+		// .andExpect(model().attribute("sha256",
+		// "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
+		// .andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
 
-    }
+	}
 
 }
